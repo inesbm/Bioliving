17065e2de58538ceb519aab3e3563b627eccd6f0
CR <ribeiro.csa@ua.pt>
2017-05-22 15:51:10 +0100

    Criação dos ficheiros "user_login", "user_register" e atualização do ficheiro "index.html".

---
+++ Projeto/public/connections/connection.php
@@ -0,0 +1,10 @@
+<?php
+//	Define	as variáveis
+$hostname = "localhost";
+$username =	"deca_16L4_03_dbo";
+$password =	"oHFyoM";
+$dbname = "deca_16L4_03";
+
+//	Estabelece	a ligação	ao	MySQL	e	BD
+$link = mysqli_connect($hostname, $username, $password, $dbname) or die("Erro na ligação..." . mysqli_connect_error());
+mysqli_set_charset($link, "utf8");
