3aebb462598b2e4323b43060bb9eaf58c7c2945c
Inês <i.margarido@ua.pt>
2017-05-22 16:13:35 +0100

    Inicialização. Elementos Base.

--- Projeto/public/pages/index.php
+++ Projeto/public/pages/index.php
@@ -4,8 +4,7 @@
     <!--Import Google Icon Font-->
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="../../../../materialize/css/materialize.min.css" media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="../../css/costume.css" media="screen,projection"/>
+    <?php include_once "../helpers/meta.php"?>
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -358,7 +357,7 @@
 
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="../../../../materialize/js/materialize.min.js"></script>
+<script type="text/javascript" src="../../materialize/js/materialize.min.js"></script>
 <script>
 
     //Initialize jQuer
