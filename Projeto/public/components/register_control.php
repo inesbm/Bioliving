cfd40eb7f0b7bb8f3c6fa04e2c927848276f0d80
CR <ribeiro.csa@ua.pt>
2017-05-25 17:48:16 +0100

    Criação do componente "register_control.php"; Correções à página "user_register.php".

---
+++ Projeto/public/components/register_control.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('../connections/connection.php');
+
+$insertSQL = "INSERT INTO bioliving_users (nome, apelido, username, email, password) VALUES (?,?,?,?,?)";
+
+$stmt = mysqli_prepare($link, $insertSQL);
+
+mysqli_stmt_bind_param($stmt, 'sssss', $nome, $apelido, $username, $email, $password);
+
+$nome = $_POST['nome'];
+$apelido = $_POST['apelido'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+if (mysqli_stmt_execute($stmt)) {
+    mysqli_stmt_close($stmt);
+    //	Acção	de	sucesso
+} else {
+    mysqli_stmt_close($stmt);
+    //	Acção	de	erro
+}
--- Projeto/public/components/user_register.php
+++ Projeto/public/components/user_register.php
@@ -5,55 +5,38 @@
     <form class="col s12">
         <div class="row">
             <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate">
+                <input id="first_name" type="text" class="validate" name="nome">
                 <label for="first_name">Nome</label>
             </div>
             <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate">
-                <label for="last_name">Sobrenome</label>
+                <input id="last_name" type="text" class="validate" name="apelido">
+                <label for="last_name">Apelido</label>
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
-                <input id="email" type="email" class="validate">
+                <input id="username" type="text" class="validate" name="username">
+                <label for="username">Username</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <input id="email" type="email" class="validate" name="email">
                 <label for="email">Email</label>
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
-                <input id="password" type="password" class="validate">
+                <input id="password" type="password" class="validate" name="password">
                 <label for="password">Password</label>
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
-                <input id="password" type="password" class="validate">
+                <input id="password_confirmation" type="password" class="validate">
                 <label for="password">Confirmação da Password</label>
             </div>
         </div>
         <a class="waves-effect waves-light btn green" type="submit" name="action">Submeter</a>
     </form>
 </div>
-
-
-
-<!--<div id="register" class="col s12">-->
-<!--    <div class="row">-->
-<!--        <form class="col s12">-->
-<!--            <div class="input-field col s12">-->
-<!--                <input id="email" type="email" class="validate">-->
-<!--                <label for="email">Email</label>-->
-<!--            </div>-->
-<!--            <div class="input-field col s12">-->
-<!--                <input id="password" type="password" class="validate">-->
-<!--                <label for="password">Password</label>-->
-<!--            </div>-->
-<!--            <p>-->
-<!--                <input type="checkbox" class="remember" id="remember" checked="checked"/>-->
-<!--                <label for="remember">Lembrar-me</label>-->
-<!--            </p>-->
-<!--            <a class="waves-effect waves-light btn green" type="submit" name="action">Entrar</a>-->
-<!--            <a class="waves-effect waves-light btn-flat" type="submit" name="action">Esqueceu-se da password?</a>-->
-<!--        </form>-->
-<!--    </div>-->
-<!--</div>-->
