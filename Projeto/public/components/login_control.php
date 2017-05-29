5cda6ffb115f2a2ec4b3bdced744f4dce9cbae64
CR <ribeiro.csa@ua.pt>
2017-05-29 15:25:53 +0100

    Correções à página "login_control.php": validação do campo "email" e redirecionar para a página de perfil caso o login seja efetuado com sucesso.

--- Projeto/public/components/login_control.php
+++ Projeto/public/components/login_control.php
@@ -3,19 +3,20 @@
 require_once('../connections/connection.php');
 
 //VALIDAÇÃO DO LOGIN
-$username =	$_POST['username'];
+$email = $_POST['email'];
 
-$query = "SELECT id_users, ref_id_roles, password FROM bioliving_users WHERE username=?";
+$query = "SELECT id_users, ref_id_roles, password, email FROM bioliving_users WHERE email=?";
 
 $result	= mysqli_prepare($link,	$query);
 
-mysqli_stmt_bind_param($result, 's', $username);
+mysqli_stmt_bind_param($result, 's', $email);
 mysqli_stmt_execute($result);
-mysqli_stmt_bind_result($result, $user_id, $role, $pass_hash);
+mysqli_stmt_bind_result($result, $user_id, $role, $pass_hash, $email);
 
 if (mysqli_stmt_fetch($result))	{
     if (password_verify($_POST['password'],	$pass_hash)) {
         //	Guardar	dados	do	utilizador	em	sessão	e	acção	de	sucesso
+        header("Location: ../pages/perfil.php");
     } else {
         //	Acção	de	erro	nos	dados	de	login
     }
