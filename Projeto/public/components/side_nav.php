4a0ce4ef9d1b6bf8db409893f2326c139e0c5452
claudia-amaro <claudia.rafaela.amaro@gmail.com>
2017-05-29 15:16:20 +0100

    Alteração de nomes para português

--- Projeto/public/components/side_nav.php
+++ Projeto/public/components/side_nav.php
@@ -4,20 +4,20 @@
             <div class="background">
                 <img src="../../images/back.png" style="width: 100%;">
             </div>
-            <a href="perfil.php"><img class="circle" src="../../images/user.jpg"></a>
+            <a href="profile.php"><img class="circle" src="../../images/user.jpg"></a>
             <a href="#"><span class="white-text name">Associação Bioliving</span></a>
             <a href="#"><span class="white-text email">geral.bioliving@gmail.com</span></a>
         </div>
     </li>
-    <li><a href="info_projeto.php"><i class="material-icons">nature_people</i>O Projeto</a></li>
+    <li><a href="info_project.php"><i class="material-icons">nature_people</i>O Projeto</a></li>
     <li><a href="moments_register.php"><i class="material-icons">add_circle</i>Registar Momento</a></li>
     <li><a href="moments.php"><i class="material-icons">photo_library</i>Momentos Recentes</a></li>
-    <li><a href="procurar_momentos.php"><i class="material-icons">search</i>Procurar Momentos</a></li>
-    <li><a href="loja.php"><i class="material-icons">local_offer</i>Loja</a></li>
+    <li><a href="moments_find.php"><i class="material-icons">search</i>Procurar Momentos</a></li>
+    <li><a href="store.php"><i class="material-icons">local_offer</i>Loja</a></li>
     <li><a href="info_bioliving.php"><i class="material-icons">info</i>Associação BioLiving</a></li>
     <li>
         <div class="divider"></div>
     </li>
-    <li><a href="perfil.php"><i class="material-icons">person</i>Perfil</a></li>
+    <li><a href="profile.php"><i class="material-icons">person</i>Perfil</a></li>
     <li><a href="#"><i class="material-icons">directions_walk</i>Logout</a></li>
 </ul>
