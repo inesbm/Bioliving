86405f3dc5e4bce1e702c2dea9b76bb978109a82
CR <ribeiro.csa@ua.pt>
2017-06-02 14:47:58 +0100

    Criação dos componentes: - "follow.moments.php" - "my.moments.php" - "profile_personal_data.php"
    Criação da página:
    - "profile.php"

--- Projeto/public/components/follow_moments.php
+++ Projeto/public/components/follow_moments.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: CR
- * Date: 02-06-2017
- * Time: 14:41
- */
--- Projeto/public/components/profile_personal_data.php
+++ Projeto/public/components/profile_personal_data.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: CR
- * Date: 01-06-2017
- * Time: 23:18
- */
