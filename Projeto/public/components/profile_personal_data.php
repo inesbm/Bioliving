3cd5d8fe56515f947e76cba594bb5a7398865e42
CR <ribeiro.csa@ua.pt>
2017-06-04 20:34:43 +0100

    Alteração ao componente "profile_personal_data.php": - inclusão do avatar em função do género do utilizador com sessão ativa; - inclusão no componente dos elementos constantes na BD.

--- Projeto/public/components/profile_personal_data.php
+++ Projeto/public/components/profile_personal_data.php
@@ -1,21 +1,23 @@
 <!--Avatar-->
 <div id="profile_photo" class="col s12">
     <div class="row">
-            <?php
-            if (isset($_SESSION['user'])) {
-                $genero = $_SESSION['genero'];
-                if ($genero == "m") {
-                    $avatar = "avatar_man.png";
-                } else {
-                    $avatar = "avatar_woman.png";
-                }
-                echo "
+        <?php
+        if (isset($_SESSION['user'])) {
+            $genero = $_SESSION['genero'];
+            $email = $_SESSION['user'];
+            $result = "";
+            if ($genero == "m") {
+                $avatar = "avatar_man.png";
+            } else {
+                $avatar = "avatar_woman.png";
+            }
+            echo "
                     <div class=\"userView\">
                         <img src=\"../../images/$avatar\" class=\"circle\" height=\"100px\">
                     </div>
                 ";
-            }
-            ?>
+        }
+        ?>
         <div id="personal_data" class="input-field-photo">
             <div class="btn-floating btn waves-effect waves-light green file-field">
                 <i class="material-icons">mode_edit</i>
@@ -32,102 +34,127 @@
 <form class="col s12" action="../components/profile_data_control.php" method="post">
     <div id="profile_data" class="col s12">
         <div class="row">
-            <div class="input-field col s6">
-                <input id="first_name" type="text" class="validate" name="nome" value="<?php $nome_BD ?>">
-                <label for="first_name">Nome</label>
+
+                <?php
+                // Ligação à BD 
+                require_once('../connections/connection.php');
+
+                // Definir a query
+                $query = "SELECT nome, apelido, genero, email, data_nascimento, rua, numero_porta, andar, codigo_postal, cidade FROM users WHERE email=?";
+                $result = mysqli_prepare($link, $query);
+                // Extrair dados da BD 
+                mysqli_stmt_bind_param($result, 's', $email);
+                mysqli_stmt_execute($result);
+                mysqli_stmt_bind_result($result, $nome, $apelido, $genero, $email, $data_nascimento, $rua, $numero_porta, $andar, $codigo_postal, $cidade);
+
+                if (mysqli_stmt_fetch($result)) {
+                    //Variáveis
+                    $nome_BD = $nome;
+                    $apelido_BD = $apelido;
+                    $genero_BD = $genero;
+                    $email_BD = $email;
+                    $data_nascimento_BD = $data_nascimento;
+                    $rua_BD = $rua;
+                    $numero_porta_BD = $numero_porta;
+                    $andar_BD = $andar;
+                    $codigo_postal_BD = $codigo_postal;
+                    $cidade_BD = $cidade;
+
+                    echo "
+            <div class=\"input-field col s6\">
+                <input id=\"first_name\" type=\"text\" class=\"validate\" name=\"nome\" value=\"$nome_BD\">
+                <label for=\"first_name\">Nome</label>
             </div>
-            <div class="input-field col s6">
-                <input id="last_name" type="text" class="validate" name="apelido">
-                <label for="last_name">Apelido</label>
+            <div class=\"input-field col s6\">
+                <input id=\"last_name\" type=\"text\" class=\"validate\" name=\"apelido\" value=\"$apelido_BD\">
+                <label for=\"last_name\">Apelido</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="username" type="text" class="validate" name="username">
-                <label for="username">Username</label>
+        <div class=\"row\">
+            <div class=\"input-field col s12\">
+                <input id=\"email\" type=\"email\" class=\"validate\" name=\"email\" value=\"$email_BD\">
+                <label for=\"email\">Email</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="email" type="email" class="validate" name="email">
-                <label for="email">Email</label>
+        <div class=\"row\">
+            <div class=\"input-field col s12\">
+                <input id=\"password\" type=\"password\" class=\"validate\" name=\"password\">
+                <label for=\"password\">Atual Password</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="password" type="password" class="validate" name="password">
-                <label for="password">Atual Password</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="password_confirmation" type="password" class="validate" name="new_password">
+        <div class=\"row\">
+            <div class=\"input-field col s12\">
+                <input id=\"new_password\" type=\"password\" class=\"validate\" name=\"new_password\">
                 <!--FALTA CONFIRMAR SE A PASSWORD É IGUAL-->
-                <label for="password">Nova Password</label>
+                <label for=\"new_password\">Nova Password</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <input id="password_confirmation" type="password" class="validate" name="cpassword">
+        <div class=\"row\">
+            <div class=\"input-field col s12\">
+                <input id=\"password_confirmation\" type=\"password\" class=\"validate\" name=\"cpassword\">
                 <!--FALTA CONFIRMAR SE A PASSWORD É IGUAL-->
-                <label for="password">Confirmação da Password</label>
+                <label for=\"password_confirmation\">Confirmação da Password</label>
             </div>
         </div>
     </div>
 
-    <div class="divider col s12">
-        <div class="row"></div>
+    <div class=\"divider col s12\">
+        <div class=\"row\"></div>
     </div>
 
-    <div id="profile_data_others" class="col s12">
-        <div class="row">
-            <div class="input-field col s12">
-                <input type="date" class="datepicker" id="calendar">
-                <label for="birthdate">Data de Nascimento</label>
+    <div id=\"profile_data_others\" class=\"col s12\">
+        <div class=\"row\">
+            <div class=\"input-field col s12\">
+                <input id=\"calendar\" type=\"date\" class=\"datepicker\" name=\"birthdate\" value=\"$data_nascimento_BD\">
+                <label for=\"calendar\">Data de Nascimento</label>
             </div>
         </div>
     </div>
 
-    <div class="divider col s12">
-        <div class="row"></div>
+    <div class=\"divider col s12\">
+        <div class=\"row\"></div>
     </div>
 
-    <div id="user_address" class="col s12">
-        <div class="row">
-            <div class="input-field col s12">
-                <input type="text" class="validate" id="rua">
-                <label for="address">Rua</label>
+    <div id=\"user_address\" class=\"col s12\">
+        <div class=\"row\">
+            <div class=\"input-field col s12\">
+                <input type=\"text\" class=\"validate\" id=\"rua\" value=\"$rua_BD\">
+                <label for=\"rua\">Rua</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s6">
-                <input type="text" class="validate" id="n_porta">
-                <label for="n_porta">N.º Porta</label>
+        <div class=\"row\">
+            <div class=\"input-field col s6\">
+                <input type=\"text\" class=\"validate\" id=\"n_porta\" value=\"$numero_porta_BD\">
+                <label for=\"n_porta\">N.º Porta</label>
             </div>
-            <div class="input-field col s6">
-                <input type="text" class="validate" id="andar">
-                <label for="andar">Andar</label>
+            <div class=\"input-field col s6\">
+                <input type=\"text\" class=\"validate\" id=\"andar\" value=\"$andar_BD\">
+                <label for=\"andar\">Andar</label>
             </div>
         </div>
-        <div class="row">
-            <div class="input-field col s6">
-                <input type="text" class="validate" id="cod_postal">
-                <label for="cod_postal">Código Postal</label>
+        <div class=\"row\">
+            <div class=\"input-field col s6\">
+                <input type=\"text\" class=\"validate\" id=\"cod_postal\" value=\"$codigo_postal_BD\">
+                <label for=\"cod_postal\">Código Postal</label>
             </div>
-            <div class="input-field col s6">
-                <input type="text" class="validate" id="cidade">
-                <label for="cidade">Cidade</label>
+            <div class=\"input-field col s6\">
+                <input type=\"text\" class=\"validate\" id=\"cidade\" value=\"$cidade_BD\">
+                <label for=\"cidade\">Cidade</label>
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="input-field col s6 center">
-            <input type="submit" name="change_profile_data" id="change_profile_data"
-                   class="waves-effect waves-light btn changes-btn green" value="Alterar">
-        </div>
-        <div class="input-field col s6 center">
-            <input type="submit" name="cancel_changes_profile_data" id="cancel_changes_profile_data"
-                   class="waves-effect waves-light btn changes-btn green" value="Cancelar">
-        </div>
+    ";
+        }
+        ?>
+<div class="row">
+    <div class="input-field col s6 center">
+        <input type="submit" name="change_profile_data" id="change_profile_data"
+               class="waves_effect waves_light btn changes-btn green" value="Alterar">
     </div>
+    <div class="input-field col s6 center">
+        <input type="submit" name="cancel_changes_profile_data" id="cancel_changes_profile_data"
+               class="waves_effect waves_light btn changes-btn green" value="Cancelar">
+    </div>
+</div>
+
 </form>
