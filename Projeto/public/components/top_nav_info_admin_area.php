f02ad094c1c00c997e4209f58baba01ad7a9b5cb
abeatrizpontess <abeatrizpontess@ua.pt>
2017-06-04 23:22:22 +0100

    Alterar nome da página de administrador

--- Projeto/public/components/top_nav_info_admin_area.php
+++ Projeto/public/components/top_nav_info_admin_area.php
@@ -1,6 +1,6 @@
 <nav class="nav-extended green">
     <div class="nav-wrapper">
-        <a href="#" class="brand-logo center">Área de Adminstrador</a>
+        <a href="#" class="brand-logo center">Área de Administrador</a>
         <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
         <!--        Menu on Desktop
                                <ul id="nav-mobile" class="right hide-on-med-and-down">
